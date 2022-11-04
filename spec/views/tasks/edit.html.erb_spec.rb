e3f758249a142e7417f74c3c639d979b8b1717ab
Fitzfoufou <francois.fitzpatrick@gmail.com>
2022-11-04 17:25:23 -0400

    feat(spec\views\tasks): correct front tests

--- spec/views/tasks/edit.html.erb_spec.rb
+++ spec/views/tasks/edit.html.erb_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'tasks/edit', type: :view do
   before(:each) do
+    @task_statuses = Task::STATUSES
     user = FactoryBot.create(:user)
     @task = assign(:task, Task.create!(
                             title: 'MyString',
@@ -16,7 +17,7 @@ RSpec.describe 'tasks/edit', type: :view do
     assert_select 'form[action=?][method=?]', task_path(@task), 'post' do
       assert_select 'input[name=?]', 'task[title]'
 
-      assert_select 'input[name=?]', 'task[status]'
+      assert_select 'select[name=?]', 'task[status]'
     end
   end
 end
--- spec/views/tasks/show.html.erb_spec.rb
+++ spec/views/tasks/show.html.erb_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'tasks/show', type: :view do
     user = FactoryBot.create(:user)
     @task = assign(:task, Task.create!(
                             title: 'Title',
-                            status: 2,
+                            status: Task::IN_PROGRESS,
                             user:
                           ))
   end
@@ -13,6 +13,6 @@ RSpec.describe 'tasks/show', type: :view do
   it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Title/)
-    expect(rendered).to match(/2/)
+    expect(rendered).to match(/In progress/)
   end
 end
